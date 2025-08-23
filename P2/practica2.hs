778cf3f06212f0b17c56d766a881624e7e1c455e
evanrock520-ciencias <evanrock520l@ciencias.unam.mx>
2025-08-23 17:14:20 -0600

    Subiendo la practica 2, aun sin comentarios y la función extra

---
+++ P2/practica2.hs
@@ -0,0 +1,41 @@
+sayHello :: String -> String
+sayHello x = "Hello, " ++ x
+
+calcularPropina :: Float -> Float
+calcularPropina x = (x*10)/100
+
+menor :: Int -> Int -> Int -> Int
+menor x y z = 
+    if x > y && x > z
+        then x
+    else 
+        if y > z
+            then y
+            else z
+
+decide ::  Bool -> String -> String -> String
+decide x y z = 
+    if x == True
+        then y
+        else z
+
+esDescendiente :: Int -> Int -> Int -> Int -> Bool
+esDescendiente a b c d = 
+    if a > b && b > c && c > d
+        then True
+        else False
+
+esDivisible :: Int -> Int -> String
+esDivisible x y =
+    if y /= 0
+        then show(x) ++ " es divisible entre " ++ show(y)
+        else "No se puede dividir entre 0"
+
+hipotenusa :: Float -> Float -> Float
+hipotenusa x y = sqrt((x ** 2) + (y ** 2))
+
+pendiente :: (Float, Float) -> (Float, Float) -> Float
+pendiente (x1,y1) (x2,y2) = (y2 - y1) / (x2 - x1)
+
+distanciaPuntos :: (Float, Float) -> (Float, Float) -> Float
+distanciaPuntos (x1, y1) (x2, y2) = sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
