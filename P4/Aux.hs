e8876d27d4db7c33346f07b9e6f78d00acdf7082
evanrock520-ciencias <evanrock520l@ciencias.unam.mx>
2025-09-20 14:48:32 -0600

    feat: Update Aux.hs

--- P4/Aux.hs
+++ P4/Aux.hs
@@ -2,9 +2,23 @@ module Aux (sumaNatural, multiNatural) where
 
 import Tipos (Natural(..))
 
+{-
+    Función: sumaNatural
+    Descripción: Suma dos números naturales.
+    Uso: sumaNatural (S (S (Cero))) (S (Cero)) = sumaNatural (S (S (Cero))) (S (Cero))
+-}
+
 sumaNatural :: Natural -> Natural -> Natural
 sumaNatural Cero n = n
 sumaNatural (S m) n = sumaNatural m (S n)
+
+{-
+    Función: multiNatural
+    Descripción: Multiplica dos números naturales
+    Uso: multiNatural (S (S (S (Cero)))) (S (S (Cero))) = S (S (S (S (S (S Cero)))))
+
+
+-}
 
 multiNatural :: Natural -> Natural -> Natural
 multiNatural Cero _ = Cero
