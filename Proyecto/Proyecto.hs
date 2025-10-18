737ddd22824105973cb26f81c81fbb461c7d38fd
evanrock520-ciencias <evanrock520l@ciencias.unam.mx>
2025-10-17 20:18:55 -0600

    feat: añadiendo las funciones crearArbol y arbol de huffman

--- Proyecto/Proyecto.hs
+++ Proyecto/Proyecto.hs
@@ -2,7 +2,7 @@
 import Aux(ordena, Dict(..))
 
 -- Definición de Árbol
-data Arbol a = Vacio | Arbol a (Arbol a) (Arbol a) deriving (Eq, Show, Ord)
+data Arbol a = Vacio | Arbol Dict (Arbol Dict) (Arbol Dict) deriving (Eq, Show, Ord)
 
 
 {-
@@ -36,6 +36,27 @@ borraTodos x xs = filter (/= x) xs
 crearListaApariciones :: String -> [Dict]
 crearListaApariciones "" = []
 crearListaApariciones (x:xs) = ordena (Dict (contarApariciones x (x:xs)) x : crearListaApariciones (borraTodos x xs))
+
+{-
+    función: arbolHuffman
+    descripción: Transporta una lista de Dict a un árbol de Huffman
+    uso: ghci> arbolHuffman [Dict 8 'r', Dict 4 'a', Dict 2 'n'] = Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') Vacio (Arbol (Dict 2 'n') Vacio Vacio)) (Arbol (Dict 4 'a') Vacio Vacio)) (Arbol (Dict 8 'r') Vacio Vacio)
+-}
+
+arbolHuffman :: [Dict] -> Arbol a
+arbolHuffman [] = Vacio
+arbolHuffman (x:xs) = Arbol (Dict 0 ' ') (arbolHuffman xs) (Arbol x Vacio Vacio) 
+
+{-
+    función: creaArbol
+    descripción: Crear un árbol de Huffman a partir de una lista de Dict ordenados.
+    uso: ghci> creaArbol "cabeza" = Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') Vacio (Arbol (Dict 1 'b') Vacio Vacio)) (Arbol (Dict 1 'c') Vacio Vacio)) (Arbol (Dict 1 'e') Vacio Vacio)) (Arbol (Dict 1 'z') Vacio Vacio)) (Arbol (Dict 2 'a') Vacio Vacio)
+-}
+
+creaArbol :: String -> Arbol a
+creaArbol "" = Vacio
+creaArbol xs = arbolHuffman (crearListaApariciones xs)
+
 
 
 
