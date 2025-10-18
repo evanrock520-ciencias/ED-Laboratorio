e99e12d13cea580528db4ff90470cb2dd149ca72
evanrock520-ciencias <evanrock520l@ciencias.unam.mx>
2025-10-18 12:54:40 -0600

    feat: añadiendo las funciones de cifrado del árbol de Huffman

--- Proyecto/Proyecto.hs
+++ Proyecto/Proyecto.hs
@@ -40,30 +40,76 @@ crearListaApariciones (x:xs) = ordena (Dict (contarApariciones x (x:xs)) x : cre
 {-
     función: arbolHuffman
     descripción: Transporta una lista de Dict a un árbol de Huffman
-    uso: ghci> arbolHuffman [Dict 8 'r', Dict 4 'a', Dict 2 'n'] = Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') Vacio (Arbol (Dict 2 'n') Vacio Vacio)) (Arbol (Dict 4 'a') Vacio Vacio)) (Arbol (Dict 8 'r') Vacio Vacio)
+    uso: arbolHuffman [Dict 8 'r', Dict 4 'a'] = Arbol ' ' (Arbol ' ' Vacio (Arbol 'a' Vacio Vacio)) (Arbol 'r' Vacio Vacio)
 -}
 
-arbolHuffman :: [Dict] -> Arbol a
+arbolHuffman :: [Dict] -> Arbol Char
 arbolHuffman [] = Vacio
-arbolHuffman (x:xs) = Arbol ' ' (arbolHuffman xs) (Arbol (devuelveChar x) Vacio Vacio) 
+arbolHuffman (x:xs) = Arbol ' ' (arbolHuffman xs) (Arbol (devuelveChar x) Vacio Vacio)
 
 {-
     función: creaArbol
     descripción: Crear un árbol de Huffman a partir de una lista de Dict ordenados.
-    uso: ghci> creaArbol "cabeza" = Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') (Arbol (Dict 0 ' ') Vacio (Arbol (Dict 1 'b') Vacio Vacio)) (Arbol (Dict 1 'c') Vacio Vacio)) (Arbol (Dict 1 'e') Vacio Vacio)) (Arbol (Dict 1 'z') Vacio Vacio)) (Arbol (Dict 2 'a') Vacio Vacio)
+    uso: creaArbol "cabeza" = Arbol ' ' (Arbol ' ' (Arbol ' ' (Arbol ' ' (Arbol ' ' Vacio (Arbol 'b' Vacio Vacio)) (Arbol 'c' Vacio Vacio)) (Arbol 'e' Vacio Vacio)) (Arbol 'z' Vacio Vacio)) (Arbol 'a' Vacio Vacio)
 -}
 
-creaArbol :: String -> Arbol a
+creaArbol :: String -> Arbol Char
 creaArbol "" = Vacio
 creaArbol xs = arbolHuffman (crearListaApariciones xs)
 
 {-
     función: devuelveChar
     descripción: Regresa el char de un Dict
-    uso:
+    uso: devuelveChar (Dict 8 'e') = 'e'
 -}
 
 devuelveChar :: Dict -> Char
 devuelveChar Nada = ' ';
 devuelveChar (Dict n c) = c
 
+{-
+    función: existeEn
+    descripción: Determina si un carácter existe en un árbol
+    uso: existeEn 'b' (Arbol ' ' (Arbol ' ' Vacio (Arbol 'a' Vacio Vacio)) (Arbol 'p' Vacio Vacio)) = False
+-}
+
+existeEn :: Char -> Arbol Char -> Bool
+existeEn _ Vacio = False
+existeEn char (Arbol c izq der)
+    | char == c = True
+    | otherwise = existeEn char izq || existeEn char der
+
+{-
+    función: determinaBinario
+    descripción: Cifra una cadena a partir de un árbol de Huffman.
+    uso: determinaBinario 'p' (Arbol ' ' (Arbol ' ' Vacio (Arbol 'a' Vacio Vacio)) (Arbol 'p' Vacio Vacio))  = "1"
+-}
+
+determinaBinario :: Char -> Arbol Char -> String
+determinaBinario _ Vacio = "" 
+determinaBinario x (Arbol c izq der)
+    | x == c = "" 
+    | existeEn x izq = '0' : determinaBinario x izq 
+    | existeEn x der = '1' : determinaBinario x der 
+    | otherwise = ""
+
+{-
+    función: cifrar
+    descripción: Regresa el cifrado de una cadena.
+    uso: cifrar "papaya" (Arbol ' ' (Arbol ' ' (Arbol ' ' Vacio (Arbol 'y' Vacio Vacio)) (Arbol 'p' Vacio Vacio)) (Arbol 'a' Vacio Vacio)) = "0110110011"
+-}
+
+cifrar :: String -> Arbol Char -> String
+cifrar "" _ = ""
+cifrar _ Vacio = error "No se puede cifrar con el árbol vacío"
+cifrar (x:xs) t = determinaBinario x t ++ cifrar xs t
+
+{-
+    función: usarCifrado
+    descripción: Regresa el cifrado binario de una cadena usando el árbol de Huffman que le corresponde.
+    uso: usarCifrado "totopo" = "0110110011"
+-}
+
+usarCifrado :: String -> String
+usarCifrado "" = ""
+usarCifrado s = cifrar s (creaArbol s)
